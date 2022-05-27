fbf28bf5d9eb0ed0bc9f689bf2cf10e7d66fcc63
Gabriel Pereira <gabrielspereira2604@gmail.com>
2022-05-26 22:12:38 -0300

    Cleanup :broom: - User

--- api/src/api/controllers/UserController.js
+++ api/src/api/controllers/UserController.js
@@ -1,49 +1,5 @@
 const User = require("../repositories/database/model/User");
 
-// module.exports = {
-//     listAll: function (req, res) {
-//       UserService.getAllUsers().then(users => {
-//         res.statusCode = 200;
-//         res.set("Content-Type", "application/json");
-//         res.send(JSON.stringify(users));
-//       })
-//     },
-
-//     add: function (req, res) {
-//       UserService.addNewUser(req.body).then((status) => {
-//         res.statusCode = status.codeStatus;
-//         res.set("Content-Type", "application/json");
-//         res.send(JSON.stringify(status.status));
-//       });
-//     },
-
-//     get: function (req, res) {
-//       const userId = req.params.user_id;
-//       UserService.getUserById(userId).then((status) => {
-//         res.statusCode = status.codeStatus;
-//         res.set("Content-Type", "application/json");
-//         res.send(JSON.stringify(status.status));                    
-//       });
-//     },
-
-//     update: function (req, res) {
-//       UserService.updateUser(req.body, req.params.user_id).then((status) => {
-//         res.statusCode = status.codeStatus;
-//         res.set("Content-Type", "application/json");
-//         res.send(JSON.stringify(status.status));
-//       });
-//     },
-
-//     remove: function (req, res) {
-//       UserService.removeUserById(req.params.user_id).then((status) => {
-//         res.statusCode = status.codeStatus;
-//         res.set("Content-Type", "application/json");
-//         res.send(JSON.stringify(status.status));
-//       });
-//     }
-// }
-
-
 module.exports = {
   async listAll(request, response) {
     try {
--- api/src/api/repositories/database/model/User.js
+++ api/src/api/repositories/database/model/User.js
@@ -1,18 +1,3 @@
-// class User {
-//   constructor(userDados) {
-//     this.id = userDados.id,
-//     this.email = userDados.email,
-//     this.username = userDados.username,
-//     this.birth = userDados.birth,
-//     this.phone = userDados.phone,
-//     this.password = userDados.password,
-//     this.createdAt = userDados.createdAt,
-//     this.updatedAt = userDados.updatedAt,
-//     this.isAdmin = userDados.isAdmin,
-//     this.isActive = userDados.isActive
-//   }
-// }
-
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require("../database");
 
