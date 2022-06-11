1039670355cbf5deb434a336751ed93358e0a708
Gabriel Pereira <gabrielspereira2604@gmail.com>
2022-06-10 21:02:25 -0300

    delete user api

--- api/app.js
+++ api/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const UserRoutes = require("./src/api/routes/UserRouter");
 const RecipeRoutes = require("./src/api/routes/RecipeRouter");
 const CommentRoutes = require("./src/api/routes/CommentRouter");
 const FavouriteRoutes = require("./src/api/routes/FavouriteRouter");
@@ -11,7 +10,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(UserRoutes);
 app.use(RecipeRoutes);
 app.use(CommentRoutes);
 app.use(FavouriteRoutes);
