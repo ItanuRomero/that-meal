8aa76496b119c997356b86f895fbc7cd34ac64b4
andreguimaraes11 <57776481+andreguimaraes11@users.noreply.github.com>
2022-06-12 12:03:54 -0300

    redefinição de senha + cadastro novos users

---
+++ web/pages/signUp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <FormSignUp></FormSignUp>
+  </div>
+</template>
+
+<script>
+import FormSignUp from "../components/user/FormSignUp.vue"
+export default {
+  name: 'IndexPage',
+  components:{
+      FormSignUp
+  }
+}
+</script>
