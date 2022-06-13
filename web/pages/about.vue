8009db31e8959225d7a072ed7871f7a439863dc3
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2022-06-12 21:34:13 -0300

    create "about" page

---
+++ web/pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <main>
+    <HeaderComponent></HeaderComponent>
+    <NavbarComponent></NavbarComponent>
+    </main>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '../components/header.vue'
+import NavbarComponent from '../components/navbar.vue'
+export default {
+  name: 'AboutPage',
+  components: {
+    HeaderComponent,
+    NavbarComponent
+  },
+}
+</script>
+
+<style>
+  
+</style>
